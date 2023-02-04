c5f694bf1aa2c8b5c47be7c7f1358519bed19179
Alanna Farias <alannaafarias@gmail.com>
2023-02-03 23:04:53 -0300

    Create Problema1.py

---
+++ Problema1.py
@@ -0,0 +1,25 @@
+# Equipe: Alanna Gilcielle Mesquita de Farias e Maria Graziela Brito de Souza
+
+
+def alternate(n, seq):
+    unpaired = True
+    m = 1
+    for i in range(1, n):
+        if unpaired:
+            if seq[i] % 2 == 0:
+                unpaired = False
+                m += 1
+        else:
+            if seq[i] % 2 != 0:
+                unpaired = True
+                m += 2
+    return m if unpaired else "NAO"
+
+
+n = int(input('informe N: '))
+if n > 1:
+    seq = [int(x) for x in input('informe a sequencia: ').strip().split()]
+    print(alternate(n, seq))
+    print('%')
+else:
+    raise ValueError('Invalid value for N')
